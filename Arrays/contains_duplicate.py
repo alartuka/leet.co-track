6e4c650f86efc10baf9026c707a151952176c038
Tuka Alarbi <80016785+alartuka@users.noreply.github.com>
2023-02-21 15:23:45 -0500

    Update contains_duplicate.py

--- Arrays/contains_duplicate.py
+++ Arrays/contains_duplicate.py
@@ -4,9 +4,9 @@ class Solution(object):
     def containsDuplicate(self, nums):
         """
         Return true if any value appears at least twice in the array, and false if every element is distinct.
-        Requires:
-          1 <= nums.length <= 105
-          -109 <= nums[i] <= 109
+        :requires:
+            1 <= nums.length <= 105
+            109 <= nums[i] <= 109
         :type nums: List[int]
         :rtype: bool
         """
