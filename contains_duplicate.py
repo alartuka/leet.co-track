44690451dd4c2ff0f5c48e07607c4757236e5831
Tuka Alarbi <80016785+alartuka@users.noreply.github.com>
2023-02-21 14:05:05 -0500

    Create contains_duplicate.py

---
+++ contains_duplicate.py
@@ -0,0 +1,14 @@
+## Link to problem: https://leetcode.com/problems/contains-duplicate/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        Return true if any value appears at least twice in the array, and false if every element is distinct.
+        Requires:
+          1 <= nums.length <= 105
+          -109 <= nums[i] <= 109
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        return not(len(nums) == len(set(nums)))
