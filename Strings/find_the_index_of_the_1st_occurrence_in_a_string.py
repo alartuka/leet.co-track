bcedeaa3f168d34082fd1d67a83f3a2e030af757
Tuka Alarbi <80016785+alartuka@users.noreply.github.com>
2024-05-26 16:22:07 -0400

    Update find_the_index_of_the_1st_occurrence_in_a_string.py

--- Strings/find_the_index_of_the_1st_occurrence_in_a_string.py
+++ Strings/find_the_index_of_the_1st_occurrence_in_a_string.py
@@ -1,4 +1,4 @@
-## Link to the problem:
+## Link to the problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
 
 class Solution(object):
     def strStr(self, haystack, needle):
