836ea7ab76f5c9f98b6503e382bfa3ae05cd271d
Tuka Alarbi <80016785+alartuka@users.noreply.github.com>
2024-05-26 16:19:26 -0400

    Create find_the_index_of_the_1st_occurrence_in_a_string.py

---
+++ Strings/find_the_index_of_the_1st_occurrence_in_a_string.py
@@ -0,0 +1,13 @@
+## Link to the problem:
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
+
+
+
